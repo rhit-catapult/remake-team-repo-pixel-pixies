fc4fe416b271f9d7b2e72b7ac6f6619e5750d6d0
Sofia James <fifijam80@gmail.com>
2025-07-12 07:49:04 -0700

    background

--- documents/sofia.py
+++ documents/sofia.py
@@ -1,1 +1,12 @@
-hi
+#hi
+import pygame
+import sys
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(640, 480)
+    background = pygame.image.load("Background.png")
+    background = pygame.transform.scale(background, (640, 480))
+    screen.blit(background, (0, 0))
+
+main()
