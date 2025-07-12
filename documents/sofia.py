d63a9aa10e3a03595a84d7938a0aff72065d79a3
Sofia James <fifijam80@gmail.com>
2025-07-12 13:37:50 -0700

    added background!

--- documents/sofia.py
+++ documents/sofia.py
@@ -4,9 +4,16 @@ import sys
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(640, 480)
-    background = pygame.image.load("Background.png")
-    background = pygame.transform.scale(background, (640, 480))
+    screen = pygame.display.set_mode((735, 412))
+    background = pygame.image.load("Background.jpg")
     screen.blit(background, (0, 0))
+    background = pygame.transform.scale(background, (735, 412))
+    pygame.display.update()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 main()
