4f1af3c4d9a71633dcaa99a36a1d7ce905a21e54
rhit-LauJ <LauJT@rose-hulman.edu>
2025-07-14 13:13:01 -0400

    Ignore

--- documents/DFplatforms.py
+++ documents/DFplatforms.py
@@ -1,15 +1,14 @@
 import pygame
 import random
 
+player = pygame.transform.scale(pygame.image.load('Fairy.png'),(800,600))
+
 
 #platforms
-class Platforms:
-    def __init__(self, screen, x, y):
-        self.screen = screen
-        self.x = x
-        self.y = y
+# class Platforms:
+#     def __init__(self, screen, x, y):
+#         self.screen = screen
+#         self.x = x
+#         self.y = y
+#
 
-def draw(self):
-    pygame.draw.rect(self.screen, (255,255,255), (10, 3, 3, 2, 2))
-
-Platforms.draw()
