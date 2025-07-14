4751fd9368348133f8bea8d7be5d5d5cd7a9c64a
Sofia James <fifijam80@gmail.com>
2025-07-14 11:42:59 -0700

    Update DFstart.py

--- documents/DFstart.py
+++ documents/DFstart.py
@@ -22,7 +22,17 @@ class StartButton:
 
     def draw(self):
         as_image = self.font.render(" Start ", True, (255, 255, 255), (56, 118, 29))
-        self.screen.blit(as_image, (300, 200))
+        self.screen.blit(as_image, (self.screen.get_width() / 2 - as_image.get_width() / 2,
+                                    self.screen.get_height() / 2 - as_image.get_height() / 2))
+
+def distance(point1, point2):
+    point1_x = point1[0]
+    point2_x = point2[0]
+    point1_y = point1[1]
+    point2_y = point2[1]
+
+    distance = math.sqrt((point1_x - point2_x)**2 + (point1_y - point2_y)**2)
+    return distance
 
 def main():
     pygame.init()
@@ -40,9 +50,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
             pressed_keys = pygame.key.get_pressed()
             if pressed_keys[pygame.K_UP]:
                 scoreboard.score = scoreboard.score + 100
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_position = event.pos
+
+
 
         scoreboard.draw()
         pygame.display.update()
