40cc83daff469e66fa1a9ce48f42a7ed545dc726
rhit-LauJ <LauJT@rose-hulman.edu>
2025-07-14 11:48:10 -0400

    Attempts at platforms

--- documents/DFplatforms.py
+++ documents/DFplatforms.py
@@ -1,1 +1,15 @@
-"Hello team!"
+import pygame
+import random
+
+
+#platforms
+class Platforms:
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+
+def draw(self):
+    pygame.draw.rect(self.screen, (255,255,255), (10, 3, 3, 2, 2))
+
+Platforms.draw()
