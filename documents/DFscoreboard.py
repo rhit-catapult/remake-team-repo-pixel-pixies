50997e9ab8c463ba12b249a960b167030a6ab601
Sofia James <fifijam80@gmail.com>
2025-07-15 11:35:24 -0700

    Update DFscoreboard.py

--- documents/DFscoreboard.py
+++ documents/DFscoreboard.py
@@ -13,14 +13,11 @@ class Fairy:
         self.image.set_colorkey((255,255,255))
         self.image = pygame.transform.scale(self.image, (150, 150))
 
-
     def draw(self):
         self.screen.blit(self.image, (self.x,self.y))
 
-
     def move(self,movement):
         self.x = self.x + movement
-
 
         left_bound = 0 - self.image.get_width() * 0.2
         right_bound =  self.screen.get_width() - self.image.get_width() + self.image.get_width() * 0.3
@@ -37,6 +34,11 @@ class Fairy:
         if self.y < self.screen.get_height()- self.image.get_height():
             self.y = self.y + Gravity_amount
 
+    def Grab (self, dust):
+        hero_grab = pygame.Rect(self.x, self.y, 50, 50)
+        return hero_grab.collidepoint((dust.x, dust.y))
+
+
 class Scoreboard:
     def __init__(self, screen):
         self.screen = screen
@@ -51,6 +53,7 @@ class Scoreboard:
         as_image = self.font.render(f" Score: {self.score} ", True, (255, 255, 255), (0, 0, 0))
         self.screen.blit(as_image, (5, 5))
 
+
 class pixiedust:
     def __init__(self, image, x, y):
         self.image = image
@@ -62,51 +65,55 @@ class pixiedust:
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((735, 415))
-    background = pygame.image.load("Background.jpg")
-    screen.blit(background, (0, 0))
-    testfairy = Fairy(screen,200,250,"Fairy.png")
-
-
-    scoreboard = Scoreboard(screen)
-    clock = pygame.time.Clock()
-
     pygame.display.set_caption("Dahlia's Flight")
-    image1 = pygame.image.load("pixiedust.png")
-    dust = pixiedust(image1, random.randint(25, 700), 300)
-    # pickup_sound = pygame.mixer.Sound("pickupPD.wav")
+    background = pygame.image.load("Background.jpg")
+    clock = pygame.time.Clock()
     WHITE = pygame.Color("white")
     BLACK = pygame.Color("black")
 
+    scoreboard = Scoreboard(screen)
+    testfairy = Fairy(screen,20,250,"Fairy.png")
+    image1 = pygame.image.load("pixiedust.png")
+    dust = pixiedust(image1, random.randint(50, 675), random.randint(100, 300))
+    items = []
+    items.append(dust)
+    pickup_sound = pygame.mixer.Sound("pickupPD.wav")
 
     while True:
+        screen.blit(background, (0, 0))
         clock.tick(60)
+        pressed_keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # pressed_keys = pygame.key.get_pressed()
             # if pressed_keys[pygame.K_UP]:
             #     scoreboard.score = scoreboard.score + 100
-            if event.type == pygame.KEYDOWN:
-                pressed_keys = pygame.key.get_pressed()
-            pressed_keys = pygame.key.get_pressed()
-            if pressed_keys[pygame.K_SPACE]:
-                testfairy.Jump(-100)
-            if pressed_keys[pygame.K_LEFT]:
-                testfairy.move(-5)
 
-            if pressed_keys[pygame.K_RIGHT]:
-                testfairy.move(5)
+        if pressed_keys[pygame.K_UP]:
+            testfairy.Jump(-20)
+        if pressed_keys[pygame.K_LEFT]:
+            testfairy.move(-5)
+        if pressed_keys[pygame.K_RIGHT]:
+            testfairy.move(5)
 
-            testfairy.MagicGravity(5)
-            testfairy.draw()
+        for item in items:
+            item.draw(screen)
+        if testfairy.Grab(dust):
+            if dust in items:
+                items.remove(dust)
+                pygame.mixer.Sound.play(pickup_sound)
+                scoreboard.score = scoreboard.score + 100
 
-        dust.draw(screen)
+        testfairy.MagicGravity(5)
+        testfairy.draw()
         scoreboard.draw()
         pygame.display.update()
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
-main()
+    main()
