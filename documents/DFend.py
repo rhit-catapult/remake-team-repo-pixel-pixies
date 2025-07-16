c17508962e624e09f686ffb66c7a320c91e8e44b
Sofia James <fifijam80@gmail.com>
2025-07-16 06:59:26 -0700

    Update DFend.py

--- documents/DFend.py
+++ documents/DFend.py
@@ -1,13 +1,30 @@
 import pygame
 import sys
 
+class Portal:
+    def __init__(self, screen, x, y, image):
+        self.screen = screen
+        self.x = x
+        self.y = y
+
+        #self.image = pygame.image.load("portal.png")
+        # self.image.set_colorkey((255, 255, 255))
+        # self.image = pygame.transform.scale(self.image, (150, 150))
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((735, 415))
     background = pygame.image.load("Portal_Place_Pixelated.png")
+    portal = Portal(screen, 100, 100, "portal.png")
     IMAGE_HEIGHT = 415
     IMAGE_WIDTH = 735
     background = pygame.transform.scale(background, (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    portal = pygame.image.load("Portal_Place_Pixelated.png")
 
     clock = pygame.time.Clock()
 
@@ -18,6 +35,7 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        portal.draw()
         pygame.display.update()
 
 main()
